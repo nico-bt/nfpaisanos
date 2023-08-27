0fab5b0a17c81e8880610c1608d80721e49b6f5f
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-08-27 20:22:10 -0300

    Get data from api. Display it. First draft with navbar

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import { symbolName } from "typescript"
+import Logo from "./Logo"
+import styles from "./styles.module.css"
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div>
+        <span>
+          <Logo />
+          <h1>NFPaisanos</h1>
+        </span>
+        <span className={styles.divider}></span>
+        <ul>
+          <li>Discover</li>
+          <li>What we do</li>
+        </ul>
+      </div>
+      <div>Connect Wallet</div>
+    </nav>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,34 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css"
 
-export default function Home() {
+const getPopularNfts = async (): Promise<NFPAISANO[]> => {
+  const res = await fetch(`${process.env.BASE_URL}/nfpaisanos/aunctions`, {
+    headers: {
+      apiKey: process.env.API_KEY as string,
+    },
+  })
+
+  if (!res.ok) {
+    throw new Error("Ups... Wrong response. Please try again")
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const popularNfts = await getPopularNfts()
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="nfts-container">
+        {popularNfts.map((item) => (
+          <article key={item.id}>
+            <img src={item.media.image} alt="imagen nft" />
+            <h2>{item.author}</h2>
+            <p>Stock: {item.stock}</p>
+            <p>Price: {item.instantPrice}</p>
+          </article>
+        ))}
+      </section>
     </main>
   )
 }
