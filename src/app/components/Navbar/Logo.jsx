06fca17cd0c3bc5df8cfe04f26ff52ecf5764faa
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-08-29 22:22:11 -0300

    fix typo

--- src/app/components/Navbar/Logo.jsx
+++ src/app/components/Navbar/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g clip-path="url(#clip0_2_663)">
+      <g clipPath="url(#clip0_2_663)">
         <path
           d="M5.33303 16.0009V5.33394H10.667V0H0V32H5.33303V21.333H21.333V16.0009H5.33303Z"
           fill="white"
