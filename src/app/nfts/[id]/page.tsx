19e7dbce5bb9ccd1838e9823248c82a52d317274
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-09-01 01:04:38 -0300

    Add items to display and styling

--- src/app/nfts/[id]/page.tsx
+++ src/app/nfts/[id]/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link"
 import arrowBack from "./arrow_back.svg"
 import Image from "next/image"
+import styles from "./styles.module.css"
 
 const getNftById = async (id: string): Promise<NFPAISANO> => {
   const res = await fetch(`${process.env.BASE_URL}/nfpaisanos/aunctions`, {
     headers: {
       apiKey: process.env.API_KEY as string,
     },
+    next: { revalidate: 10 },
   })
 
   if (!res.ok) {
@@ -22,13 +24,11 @@ export default async function page({ params }: { params: { id: string } }) {
   const nft = await getNftById(params.id)
 
   return (
-    <div style={{ display: "grid", justifyContent: "center" }}>
-      <article
-        style={{ display: "grid", gridTemplateColumns: "2fr 1fr", placeItems: "center", gap: 12 }}
-      >
+    <div className={styles.grid}>
+      <article>
         <img src={nft.media.image} alt="imagen nft" style={{ maxHeight: "90vh" }} />
 
-        <div style={{ display: "grid", gap: 6 }}>
+        <div className={styles.body}>
           <img
             src={nft.authorAvatar}
             alt="author avatar"
@@ -36,11 +36,28 @@ export default async function page({ params }: { params: { id: string } }) {
             style={{ height: 60, width: 60, marginInline: "auto" }}
           />
           <h2 style={{ textAlign: "center" }}>{nft.author}</h2>
-          <p>Stock: {nft.stock}</p>
-          <p>Price: {nft.instantPrice}</p>
-          <p>Likes: {nft.likes} ❤️ </p>
-          <p>Created at: {new Date(nft.createdAt).toDateString()}</p>
-          <p>Type: {nft.type}</p>
+          <p>
+            Stock: <span className={styles.biggerFont}>{nft.stock}</span>
+          </p>
+          <p className={styles.price}>{nft.instantPrice}</p>
+          <p className={styles.biggerFont}>❤️ {nft.likes}</p>
+
+          <p>
+            Highest bid
+            <span className={styles.biggerFont}>{nft.highestBid}</span>
+          </p>
+
+          <p>
+            Created at:
+            <span className={styles.biggerFont}>{new Date(nft.createdAt).toDateString()}</span>
+          </p>
+
+          <p>
+            Type:
+            <span className={styles.biggerFont}>{nft.type}</span>
+          </p>
+
+          <p className={styles.type}>{nft.attributes.type}</p>
 
           <Link href="/" style={{ justifySelf: "flex-end", marginTop: 40 }}>
             <Image
